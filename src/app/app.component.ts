69edb81e69e41c3cfe2d59c067abac0f7e8d37e0
Marco Tulio Linares <macolinares@gmail.com>
2017-11-11 10:53:19 -0600

    Creación del proyecto inicial, sin ningun estilo. Funciona sin base de datos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+
+	tareas : string[] = [];
+
+	agregar(tarea : string) {
+		if(tarea) {
+			this.tareas.push(tarea);
+		}
+	}
+
+	eliminar(tarea : string) {
+		if(tarea) {
+			const index: number = this.tareas.indexOf(tarea);
+			if (index !== -1) {
+				this.tareas.splice(index, 1);
+			}    
+		}
+	}
 }
